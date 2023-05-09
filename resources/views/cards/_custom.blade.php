6644f33638abb9bd82066646cb199d89aacef932
Achraf Khouadja <achraf.khouadja@y1.de>
2023-05-09 20:56:55 +0200

    first commit

---
+++ resources/views/cards/_custom.blade.php
@@ -0,0 +1,6 @@
+<div class="card child mb-3 mt-3 custom-card" data-widget="{{ $widget->toJson()  }}">
+    <div class="card-header text-center">{{ $widget->title }}</div>
+    <div class="card-body">
+            This is a Custom widget that can be developed further
+    </div>
+</div>
